b1907a9d4457ebc51b44a0abfd71e934614a567c
Zabrane <zabrane3@gmail.com>
2024-11-01 07:39:57 +0100

    Delete vsn.mk

--- vsn.mk
+++
@@ -1,6 +0,0 @@
-# Copyright 2012-2024 Tail-f Systems AB
-#
-# See the file "LICENSE" for information on usage and redistribution
-# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
-
-LUX_VSN=3.0
